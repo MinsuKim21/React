cfd8ae3da4017f3a7e6d377162ae3194b0048109
MinsuKim <minsu020302@gmail.com>
2022-06-23 07:50:56 +0900

    Chore: How to use React state

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
+import Counter from './Counter';
 
 function App() {
     const number = 5;
@@ -8,11 +9,7 @@ function App() {
     return (
         <div className="App">
             <MyHeader />
-            <h2>안녕리엑트</h2>
-            <b id="bold_text">
-                {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-            </b>
-            <MyFooter />
+            <Counter />
         </div>
     );
 }
