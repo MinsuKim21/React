290f76780e41172eba3fdd589c88ccf36e787f44
MinsuKim <minsu020302@gmail.com>
2022-06-23 07:29:45 +0900

    Chore: conditional rendering(조건부랜더링)

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕리엑트</h2>
-      </header>
-    </div>
-  );
+    const number = 5;
+
+    return (
+        <div className="App">
+            <MyHeader />
+            <h2>안녕리엑트</h2>
+            <b id="bold_text">
+                {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
+            </b>
+            <MyFooter />
+        </div>
+    );
 }
 
 export default App;
